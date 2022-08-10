fbe0a8fff86870f4dd1af960fb01f57a15d3f534
Alek Ivankovic <alek.ivankovic@rac.com.au>
2022-08-10 12:08:21 +0800

    Changed name and added tags to backend

--- infrastructure/backends.tf
+++ infrastructure/backends.tf
@@ -1,7 +1,8 @@
 locals {
   tf_backends = {
     backstage_test : {
-      name = "backstage-testing"
+      name = "backstage-test"
+      repo = "https://github.com/bortington/backstage-test"
     }
   }
 }
@@ -11,4 +12,8 @@ resource "azurerm_storage_container" "tf_backends_sc" {
   name                  = each.value.name
   container_access_type = "private"
   storage_account_name  = azurerm_storage_account.terraform_state_storage.name
+
+  tags = {
+    Repo_URL = each.value.repo
+  }
 }
