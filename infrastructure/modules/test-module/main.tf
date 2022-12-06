86e0d3fc4292504b40ad8b4334fccce1794d42fc
Alek Ivankovic <alek.ivankovic@gmail.com>
2022-12-06 11:56:40 +0800

    Create main.tf

---
+++ infrastructure/modules/test-module/main.tf
@@ -0,0 +1,15 @@
+variable "test_var" {
+  type = string
+}
+
+resource "null_resource" test {
+   triggers = {
+    name = var.test_var
+  }
+  
+  provisioner "local-exec" {
+    when        = create
+    command     = "Write-Output \"Hello ${var.test_var}\""
+    interpreter = ["pwsh", "-Command"]
+  }
+}
